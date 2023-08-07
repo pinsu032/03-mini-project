90cd51a3659b37c9eb338ba4841fbb2dfe023fb6
ujjawal.kinker <ujjawal.kinker@PUNITP639448L.ad.infosys.com>
2023-08-07 21:41:12 +0530

    save

--- src/main/java/com/ms/controller/UserController.java
+++ src/main/java/com/ms/controller/UserController.java
@@ -22,7 +22,6 @@ public class UserController {
 	public ResponseEntity<String> saveUser(@RequestBody User user){	
 		boolean status = service.registerEmployee(user);
 		
-		
 		if(status)
 		  return new ResponseEntity<String>("mail sent",HttpStatus.OK);
 		else
