cab223458dbcc9564e1f4e8183070e92505a9200
ujjawal.kinker <ujjawal.kinker@PUNITP639448L.ad.infosys.com>
2023-08-08 10:24:27 +0530

    first commit

--- src/main/java/com/ms/controller/UserController.java
+++ src/main/java/com/ms/controller/UserController.java
@@ -20,7 +20,6 @@ public class UserController {
 	
 	@PostMapping("/")
 	public ResponseEntity<String> saveUser(@RequestBody User user){
-		
 		boolean status = service.registerEmployee(user);
 		
 		if(status)
